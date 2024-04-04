a681c061cc93494ba22d8a686d1235c4b817d539
Kesav Nagendra <71759025+kesavn-13@users.noreply.github.com>
2024-04-04 14:54:12 +0530

    Update app.py
    final

--- app.py
+++ app.py
@@ -73,7 +73,8 @@ try:
               for j,mask in enumerate(result.masks.data):
                   mask = (mask.numpy() * 255).astype(np.uint8)  # Convert to uint8
                   mask_image = Image.fromarray(mask)
-                  st.image(mask, width = 640, caption=high_conf)
+                  st.image(mask, width = 640, caption="Extracted Image")
+                  st.write(high_conf)
                   cv2.imwrite("wout.png",mask)
 
 
