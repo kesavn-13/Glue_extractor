7dce4581a072a3fc2a535c398abc7e63a58e6c16
Kesav Nagendra <71759025+kesavn-13@users.noreply.github.com>
2024-04-04 12:51:07 +0530

    Update app.py
    xfhh

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ try:
              for j,mask in enumerate(result.masks.data):
                  mask = (mask.numpy() * 255).astype(np.uint8)  # Convert to uint8
                  mask_image = Image.fromarray(mask)
-                 b=result.probs
+                 b=results.to_string(result.probs)
                  st.image(mask, width = 640, caption = result.probs)
                  cv2.imwrite("wout.png",mask)
 
